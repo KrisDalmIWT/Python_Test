afd32d100c97a804f85341a3086eef297a62265a
Kris <dalm@iwt-bodensee.de>
2020-10-06 09:56:19 +0200

    Erste zwei Uebungen fertig

---
+++ ex01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+a = 10
+b = 200
+
+product = a * b
+summe = a + b
+
+if product <= 1000:
+    print "Das Produkt: " + str(product)
+else:
+    print "Die Summe: " + str(summe)
